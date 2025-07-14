80c1d1ed0d0dc10a3c1592fb61c4a9961e8bef89
Joseph Rodriguez <jarp1195@gmail.com>
2025-07-14 19:27:35 -0400

    Renamed JS file per Project requirements. Added console.log functions.

--- scripts/index.js
+++ scripts/index.js
@@ -15,17 +15,21 @@ const modalPostCloseBtn = newPostModal.querySelector(".modal__close-button");
 //edit profile events
 editProfileBtn.addEventListener("click", function () {
   editProfileModal.classList.add("modal_is-opened");
+  console.log("editProfileBtn was clicked");
 });
 
 modalProfileCloseBtn.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_is-opened");
+  console.log("modalProfileCloseBtn was clicked");
 });
 
 //new post events
 newPostBtn.addEventListener("click", function () {
   newPostModal.classList.add("modal_is-opened");
+  console.log("newPostBtn was clicked");
 });
 
 modalPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
+  console.log("modalPostCloseBtn was clicked");
 });
